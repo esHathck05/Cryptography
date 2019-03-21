23e5f74573f71fc7ad297cdfea1612141a7a1b17
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-03-21 09:33:11 -0400

    Updated from Brython Server: 3/21/2019 9:33:08 AM

--- cryptography.py
+++ cryptography.py
@@ -14,21 +14,58 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 
 question = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
-if question == 'e':
-    message = input("Message: ")
-    key = input("Key: ")
-    for x in message:
-        print(list(x))
-        #print(associations.find(x))
-    #for y in key:
-        #print(associations.find(y))
+messagenmbrs = []
+keynmbrs = []
+bothnmbrs = []
+ecommand = []
+bothdifference = []
+dcommand = []
 
-"""
-elif question == 'd':
+if question == 'e':
+    
     message = input("Message: ")
     key = input("Key: ")
+    message = list(message)
+    key = list(key)
+    
+    for x in message:
+        messagenmbrs.append(associations.find(x))
+    for y in key:
+        keynmbrs.append(associations.find(y))
+    for z in range(0, len(messagenmbrs)):
+        bothnmbrs.append(messagenmbrs[z] + keynmbrs[z%len(keynmbrs)])
+    for a in bothnmbrs:
+        ecommand.append(associations[1%85])
+    
+    print(''.join(ecommand))
+    
+    messagenmbrs = []
+    keynmbrs = []
+    bothnmbrs = []
+    ecommand = []
+
+elif question == 'd':
+    
+    emessage = input("Message: ")
+    key = input("Key: ")
+    
+    for x in emessage:
+        messagenmbrs.append(associations.find(x))
+    for y in key:
+        keynmbrs.append(associations.find(y))
+    for z in range(0, len(messagenmbrs)):
+        bothnmbrs.append(messagenmbrs[z]-keynmbrs[z%len(keynmbrs)])
+    for a in bothnmbrs:
+        dcommand.append(associations[a%85])
+    
+    print(''.join(dcommand))
+    
+    messagenmbrs = []
+    keynmbrs = []
+    bothnmbrs = []
+    dcommand = []
+
 elif question == 'q':
     print("Goodbye!")
 else:
     print("Did not understand command, try again.")
-"""
