091d9ce1194e85837b6cb04f0ecc5c7c1fb8279e
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-07-09 13:22:16 -0400

    Updated from Brython Server: 7/9/2019 1:22:15 PM

--- cryptography.py
+++ cryptography.py
@@ -1,14 +1,3 @@
-"""
-cryptography.py
-Author: Esther Hacker
-Credit: n/A
-
-Assignment: Cryptography
-
-Write and submit a program that encrypts and decrypts user data.
-
-See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
-"""
 
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
